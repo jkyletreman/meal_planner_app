1e4696cc4961eafac92ab448a4173108289f4387
Kyle Treman <jkt290@nyu.edu>
2018-01-23 14:19:33 -0500

    Add show for created recipe

--- models/recipes.js
+++ models/recipes.js
@@ -8,8 +8,18 @@ function createRecipe({ body : { title, instructions, ingredients, time, img, di
     .insert({ title, instructions, ingredients, time, img, difficulty, favorite, tags });
 }
 //Find All
+function findRecipes() {
+  return knex("recipes");
+}
 //Find One
+function findRecipe({ params: { id }}) {
+  return knex("recipes").where("id", id);
+}
 //Update
 //Destroy
 
-module.exports = { createRecipe };
+module.exports = {
+  createRecipe,
+  findRecipes,
+  findRecipe
+ };
