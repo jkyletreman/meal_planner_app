799b4203f9f2e841547af00aeb5433fe9b1cc4c4
Kyle Treman <jkt290@nyu.edu>
2018-01-23 13:54:53 -0500

    Fix dev env

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ if (process.env.APP_MODE === undefined) {
 module.exports = {
  development: {
    client: 'pg',
-   connection: process.env.DATABASE_URL
+   connection: process.env.DATABASE_URL || "postgres://localhost:5432/meal_planner"
  },
  production: {
    client: 'pg',
