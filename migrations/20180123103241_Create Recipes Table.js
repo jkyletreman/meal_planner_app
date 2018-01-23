10924f0e11297eae16fdad368327d3be982b19b5
Kyle Treman <jkt290@nyu.edu>
2018-01-23 10:35:09 -0500

    Add recipes table migration

--- migrations/20180123103241_Create Recipes Table.js
+++ migrations/20180123103241_Create Recipes Table.js
@@ -1,8 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('Recipes', (table) => {
+    table.increments();
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("Recipes");
 };
