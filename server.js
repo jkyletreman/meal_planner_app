6e4a9d490895071d8ec0046fb8102a941e94f39e
Kyle Treman <jkt290@nyu.edu>
2018-01-23 11:13:35 -0500

    Add basic RESTful

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ const knex = require('./db'),
 
 const PORT = process.env.PORT || '8000';
 
+
+// Routes
+app.get('/recipes', (req, res) => {
+  res.send('Working');
+});
+
+app.get('recipes/:id', (req, res) => {
+  res.send('Working');
+});
+
+app.post('recipes/new', (req, res) => {
+  res.send('Working');
+});
+
+app.patch('recipes/:id', (req, res) => {
+  res.send('Working');
+});
+
+app.delete('recipes/:id', (req, res) => {
+  res.send('Working');
+});
+
 app.listen(PORT, () => {
   console.log(`Running on ${PORT}`);
 });
