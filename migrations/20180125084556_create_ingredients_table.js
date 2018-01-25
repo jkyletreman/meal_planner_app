01b3f3ab4f91305c17ba9027084cd89b0b3d642b
Kyle Treman <jkt290@nyu.edu>
2018-01-25 09:12:57 -0500

    Add ingredients migrations, join table, seed folder

---
+++ migrations/20180125084556_create_ingredients_table.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('ingredients', (table) => {
+    table.increments();
+    table.string('name', 255).notNull();
+    table.string('measurement', 30).notNull();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('ingredients');
+};
